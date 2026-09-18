52848ee8390e0acf0e48ee1ae5dd9a67fb49ec62
Pol <pol.rovira04@gmail.com>
2026-09-18 16:11:10 +0200

    Vercel fix v4
    fix v4

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@sparticuz/chromium", "playwright-core"],
+  outputFileTracingIncludes: {
+    "/**/*": [
+      "./node_modules/.pnpm/playwright-core@*/node_modules/playwright-core/browsers.json",
+      "./node_modules/.pnpm/@sparticuz+chromium@*/node_modules/@sparticuz/chromium/bin/**",
+    ],
+  },
 };
 
 export default nextConfig;
